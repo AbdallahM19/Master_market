1d94ad5e444b76e025c996d452def2b0221f1630
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-06-07 14:12:39 +0200

    pycodestyle app_dynamic.py

--- app_dynamic.py
+++ app_dynamic.py
@@ -16,7 +16,9 @@ app = Flask(__name__, template_folder='dynamic_templates')
 def transform_attributes(products):
     attributes_dict = {}
     for product in products:
-        attributes_list = product['attributes'].split(', ') if 'attributes' in product else []
+        attributes_list = product[
+            'attributes'
+        ].split(', ') if 'attributes' in product else []
         attributes_dict[product['product_id']] = attributes_list
     return attributes_dict
 
@@ -24,7 +26,9 @@ def transform_attributes(products):
 def transform_images(products):
     images_dict = {}
     for product in products:
-        images_list = product['images'].split(', ') if 'images' in product else []
+        images_list = product[
+            'images'
+        ].split(', ') if 'images' in product else []
         images_dict[product['product_id']] = images_list
     return images_dict
 
@@ -32,7 +36,9 @@ def transform_images(products):
 def transform_attributes_list(products):
     attributes_dict = {}
     for product in products:
-        attributes_list = product['attributes'] if 'attributes' in product else []
+        attributes_list = product[
+            'attributes'
+        ] if 'attributes' in product else []
         attributes_dict[product['product_id']] = attributes_list
     return attributes_dict
 
@@ -46,29 +52,29 @@ def transform_images_list(products):
 
 
 def register_user(username, fullname, email, password, filename="users.json"):
-        """Register a new user"""
-        new_user = {
-            "user_id": str(uuid.uuid4()),
-            "username": username,
-            "fullname": fullname,
-            "email": email,
-            "password": password,
-            "cart": []
-        }
-        try:
-            with open(filename, "r") as file:
-                users = load(file)
-        except FileNotFoundError:
-            users = []
+    new_user = {
+        "user_id": str(uuid.uuid4()),
+        "username": username,
+        "fullname": fullname,
+        "email": email,
+        "password": password,
+        "cart": []
+    }
 
-        for user in users:
-            if user["username"] == username:
-                return
-            if user["email"] == email:
-                return
-        users.append(new_user)
-        with open("users.json", "w") as file:
-            dump(users, file, indent=4)
+    try:
+        with open(filename, "r") as file:
+            users = load(file)
+    except FileNotFoundError:
+        users = []
+
+    for user in users:
+        if user["username"] == username:
+            return
+        if user["email"] == email:
+            return
+    users.append(new_user)
+    with open("users.json", "w") as file:
+        dump(users, file, indent=4)
 
 
 def get_db_connection():
@@ -125,7 +131,11 @@ def mastermarket():
     return render_template('home.html')
 
 
-@app.route('/mastermarket/login', methods=['GET', 'POST'], strict_slashes=False)
+@app.route(
+    '/mastermarket/login',
+    methods=['GET', 'POST'],
+    strict_slashes=False
+)
 @app.route('/login', methods=['GET', 'POST'], strict_slashes=False)
 def login():
     if request.method == 'POST':
@@ -137,7 +147,9 @@ def login():
             db = get_db_connection()
             cursor = db.cursor(dictionary=True)
             cursor.execute(
-                "SELECT * FROM users WHERE (username = %s OR email = %s) AND password = %s", (
+                """SELECT * FROM users WHERE\
+                (username = %s OR email = %s)\
+                AND password = %s""", (
                     username_or_email,
                     username_or_email,
                     password
@@ -161,15 +173,29 @@ def login():
             if user:
                 return jsonify({"success": True})
             else:
-                return jsonify({"success": False, "message": "Invalid Username/E-mail or Password"})
+                return jsonify(
+                    {
+                        "success": False,
+                        "message": "Invalid Username/E-mail or Password"
+                    }
+                )
         except Exception:
             print("Error: {}".format(Exception))
-            return jsonify({"success": False, "message": "Internal server error"})
+            return jsonify(
+                {
+                    "success": False,
+                    "message": "Internal server error"
+                }
+            )
 
     return render_template('login.html')
 
 
-@app.route('/mastermarket/register', methods=['GET', 'POST'], strict_slashes=False)
+@app.route(
+    '/mastermarket/register',
+    methods=['GET', 'POST'],
+    strict_slashes=False
+)
 @app.route('/register', methods=['GET', 'POST'], strict_slashes=False)
 def register():
     if request.method == 'POST':
@@ -202,11 +228,26 @@ def register():
 
             if user:
                 if user['username'] == username and user['email'] == email:
-                    return jsonify({"success": False, "message": "Username & E-mail is Exists."})
+                    return jsonify(
+                        {
+                            "success": False,
+                            "message": "Username & E-mail is Exists."
+                        }
+                    )
                 elif user['email'] == email:
-                    return jsonify({"success": False, "message": "E-mail is Exists."})
+                    return jsonify(
+                        {
+                            "success": False,
+                            "message": "E-mail is Exists."
+                        }
+                    )
                 elif user['username'] == username:
-                    return jsonify({"success": False, "message": "Username is Exists."})
+                    return jsonify(
+                        {
+                            "success": False,
+                            "message": "Username is Exists."
+                        }
+                    )
             else:
                 register_user(username, fullname, email, password)
                 convert_from_json_to_mysql()
@@ -214,7 +255,12 @@ def register():
                 return jsonify({"success": True})
         except Exception:
             print("Error: {}".format(Exception))
-            return jsonify({"success": False, "message": "Internal server error"})
+            return jsonify(
+                {
+                    "success": False,
+                    "message": "Internal server error"
+                }
+            )
 
     return render_template('signin.html')
 
